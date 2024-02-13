44864dae437f27748b65fce7ee0532a6b749b30b
vilppu <blaanblaa123@gmail.com>
2024-02-13 11:46:09 +0200

    done

---
+++ src/main/java/fi/metropolia/Laskin.java
@@ -0,0 +1,48 @@
+package fi.metropolia;
+
+public class Laskin {
+
+    private int tulos;  	// Muuttuja tulokselle
+
+    public void nollaa() {  // Nollaa tulosmuuttuja
+        tulos = 0;
+    }
+
+    public int annaTulos() {
+        return tulos;
+    }
+
+    public void lisaa(int n) {
+        tulos = tulos + n;
+    }
+
+    public void vahenna(int n) {
+        tulos = tulos - n;
+    }
+
+    public void kerro(int n) {
+        tulos = tulos * n;
+    }
+
+    public void jaa(int n) {
+        if (n==0) throw new ArithmeticException("Nollalla ei voi jakaa");
+        tulos = tulos / n;
+    }
+
+    public void nelio(int n) {
+        tulos = n * n;
+    }
+
+    public void neliojuuri(int n) {
+        tulos = (int)Math.sqrt(n);
+    }
+
+    public void virtaON() {
+        tulos = 0;
+    }
+
+    public void virtaOFF() {
+        tulos = 0;
+    }
+
+}
